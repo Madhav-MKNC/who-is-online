eee6d10505f6b23e590bb8e3662290262c6bfc75
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-08 15:32:46 +0530

    d

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -1,3 +1,7 @@
+let res = document.getElementById('res');       
+
+// res.innerHTML = '<h1> hi </h1>'
+
 // Listen for messages from the popup
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
     // If the message is to get the URL of the current tab
