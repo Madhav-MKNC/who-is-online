a01184f3fd9da4b89bacdb3058fcc307bebe369e
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-08 15:32:11 +0530

    asdf

--- background/background.js
+++ background/background.js
@@ -3,3 +3,20 @@ chrome.action.setBadgeText(
         text: "35"
     }
 );
+
+let res = {
+    url : 'https://example.com'
+}
+
+
+chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+    if (message.type === 'get_url') {
+      sendResponse(res);
+    }
+  });
+
+
+
+// chrome.browserAction.onClicked.addListener(function(tab) {
+//     chrome.tabs.executeScript(tab.id);
+// });
