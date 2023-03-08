cc56d556d3cf8c852ea2de6c7a71999e712eeaf8
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-08 15:33:07 +0530

    af

--- js/popup.js
+++ js/popup.js
@@ -1,15 +1,20 @@
-let res = document.getElementById('res');
+let res = document.getElementById("url-display");
+
+// chrome.tabs.getCurrent(function(tab) {
+//   alert(tab.title);
+// });
+
+// alert(document.all[4].innerHTML);
+
 
 chrome.runtime.sendMessage({ type: "get_url" }, function(response) {
-    try {
-      if (!response || !response.url) {
-        throw new Error("Invalid response");
-      }
-      alert(response);
-      // res.innerText = response;
-    } catch (error) {
-      console.error("Error handling response", error);
+  try {
+    if (!response || !response.url) {
+      throw new Error("Invalid response");
     }
-  });
-
-console.log(chrome.runtime);
+    // alert(response.url);
+  } catch (error) {
+    console.error("Error handling response", error);
+  }
+  // alert(response.url);
+});
