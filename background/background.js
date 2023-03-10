03b52c14938d4e299253c40800b81a76c348ef5e
Madhav Kumar <78339236+Madhav-MKNC@users.noreply.github.com>
2023-03-10 13:19:28 +0530

    bg

--- background/background.js
+++ background/background.js
@@ -1,22 +1,38 @@
-chrome.action.setBadgeText(
-    {
-        text: "35"
-    }
-);
+// chrome.action.setBadgeText(
+//     {
+//         text: "35"
+//     }
+// );
 
-let res = {
-    url : 'https://example.com'
-}
-
-
-chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
-    if (message.type === 'get_url') {
-      sendResponse(res);
-    }
-  });
+// let res = {
+//     url : 'https://example.com'
+// }
 
 
-
-// chrome.browserAction.onClicked.addListener(function(tab) {
-//     chrome.tabs.executeScript(tab.id);
+// chrome.runtime.onMessage.addListener((message, sender, sendResponse) => {
+//   if (message.type === 'get_url') {
+//     sendResponse(res);
+//   }
 // });
+
+
+
+
+
+// testing
+
+
+chrome.runtime.onMessage.addListener((msg, sender) => {
+  // First, validate the message's structure.
+  if ((msg.from === 'content') && (msg.subject === 'showPageAction')) {
+    // Enable the page-action for the requesting tab.
+    chrome.pageAction.show(sender.tab.id);
+  }
+});
+
+
+chrome.tabs.query({active: true, currentWindow: true},function(tabs) {
+  chrome.tabs.sendMessage(tabs[0].id, {greeting: "hello"}, function(response) {
+      console.log(response);
+  });
+});
