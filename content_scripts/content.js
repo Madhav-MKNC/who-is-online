dd63b863315711313443aed7a14d9043c4a1baf6
MKNC <madhavampire@gmail.com>
2023-03-12 16:59:33 +0530

    initial commit

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -32,17 +32,17 @@
 
 // Listen for messages from the popup.
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-    // First, validate the message's structure.
-    if (msg.from === 'popup') {
-      // Collect the necessary data. 
-      // (For your specific requirements `document.querySelectorAll(...)`
-      //  should be equivalent to jquery's `$(...)`.)
+  // First, validate the message's structure.
+  if (msg.from === 'popup') {
+    // Collect the necessary data. 
+    // (For your specific requirements `document.querySelectorAll(...)`
+    //  should be equivalent to jquery's `$(...)`.)
     //   var domInfo = {
     //     url : window.location.href
     //   };
-  
-      // Directly respond to the sender (popup), 
-      // through the specified callback.
-      response(window.location.href);
-    }
-  });
+
+    // Directly respond to the sender (popup), 
+    // through the specified callback.
+    response(window.location.href);
+  }
+});
