d35e40579599227a3d2e00bed2fb3e144b288238
pio <pio@gmail.com>
2023-03-29 20:17:04 +0530

    temporary commit

--- content_scripts/content.js
+++ content_scripts/content.js
@@ -7,17 +7,14 @@
 //   subject: 'showPageAction'
 // });
 
+
 // Listen for messages from the popup.
 chrome.runtime.onMessage.addListener((msg, sender, response) => {
-  console.log('adsadf');
   // First, validate the message's structure.
   if (msg.from === 'popup') {
     // Collect the necessary data. 
     // (For your specific requirements `document.querySelectorAll(...)`
     //  should be equivalent to jquery's `$(...)`.)
-    //   var domInfo = {
-    //     url : window.location.href
-    //   };
 
     // Directly respond to the sender (popup), 
     // through the specified callback.
--- js/popup.js
+++ js/popup.js
@@ -1,28 +1,28 @@
 // testing
 
-// Update the relevant fields with the new data.
-// const setDOMInfo = info => {
-//   res.innerHTML = info.url;
-// };
-
 // Once the DOM is ready...
 window.addEventListener('DOMContentLoaded', () => {
   let res = document.getElementById("res");
   res.innerHTML = 'this is res';
   
   // ...query for the active tab...
-  // chrome.tabs.query({
-  //   active: true,
-  //   currentWindow: true
-  // }, tabs => {
-  //   // ...and send a request for the DOM info...
-  //   chrome.tabs.sendMessage(
-  //       tabs[0].id,
-  //       {from: 'popup', subject: 'DOMInfo'},
-  //       // ...also specifying a callback to be called 
-  //       //    from the receiving end (content script).
-  //       (url)=>{
-  //         res.innerHTML = url
-  //       });
-  // });
+  chrome.tabs.query({
+    active: true,
+    currentWindow: true
+  }, tabs => {
+    // ...and send a request for the DOM info...
+    chrome.tabs.sendMessage(
+        tabs[0].id,
+        {from: 'popup', subject: 'DOMInfo'},
+        // ...also specifying a callback to be called 
+        //    from the receiving end (content script).
+        (url)=>{
+          if(url=="undefined"){
+            res.innerHTML = "please try after some times";
+          }
+          else{
+            res.innerHTML = url
+          }
+        });
+  });
 });
